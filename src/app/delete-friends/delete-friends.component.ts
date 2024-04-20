70b6aac646b7b7b3c17fd3aad17d63f48f086462
Msuf123 <akshatmalik18t@gmail.com>
2024-04-20 15:02:10 +0530

    Made them functionable

--- src/app/delete-friends/delete-friends.component.ts
+++ src/app/delete-friends/delete-friends.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-delete-friends',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './delete-friends.component.html',
   styleUrl: './delete-friends.component.css'
 })
 export class DeleteFriendsComponent {
-
+  constructor(private formBuilder:FormBuilder){}
+  form=this.formBuilder.group({
+    name:['']
+  })
+  submitData(){
+    
+    console.log(this.form.value)
+  }
 }
--- src/app/update-friend/update-friend.component.ts
+++ src/app/update-friend/update-friend.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-update-friend',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './update-friend.component.html',
   styleUrl: './update-friend.component.css'
 })
 export class UpdateFriendComponent {
-
+  constructor(private formBuilder:FormBuilder){}
+  form=this.formBuilder.group({
+    name:[''],
+    place:['']
+  })
+  submitData(){
+    
+    console.log(this.form.value)
+  }
 }
