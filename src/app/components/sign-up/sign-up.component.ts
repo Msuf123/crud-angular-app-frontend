c73d626e988e4f3d0f2f4d2ce3d7188f12cf2181
Msuf123 <akshatmalik18t@gmail.com>
2024-04-25 19:47:49 +0530

    Working towards form validation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+ formBuilder=inject(FormBuilder)
+ emailError:{email:boolean,required:boolean}={email:false,required:false}
+ signUpForm=this.formBuilder.group({
+  userId:['',[Validators.required,Validators.email]]
+  ,password:['']
+ })
+ constructor(){
+  this.signUpForm.valueChanges.subscribe(()=>{
+    this.emailError=this.signUpForm.get('userId')?.hasError('required')?{email:false,required:true}:{email:false,required:false}
+    this.emailError=this.signUpForm.get('userId')?.hasError('email')?{email:true,required:false}:{email:false,required:false}
+  })
+ }
+ 
+ formSubmitted(){
+  console.log(this.signUpForm.value)
+ }
+ 
 }
