90ded32f4b35f031050fb38e3cbb690acc44bc76
Msuf123 <akshatmalik18t@gmail.com>
2024-05-09 23:43:04 +0530

    Successfully redirected but lacks a error popping

--- src/app/components/github-callback/github-callback.component.ts
+++ src/app/components/github-callback/github-callback.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FetchDataService } from '../../services/fetch-data.service';
+import { ErrorService } from '../../services/error/error.service';
 
 @Component({
   selector: 'app-github-callback',
@@ -10,14 +11,19 @@ import { FetchDataService } from '../../services/fetch-data.service';
   styleUrl: './github-callback.component.css'
 })
 export class GithubCallbackComponent {
-  constructor(private router:Router,private request:FetchDataService){}
+  constructor(private router:Router,private request:FetchDataService,private showError:ErrorService){}
   ngOnInit(){
     const code=new URL(this.router.url,'http://localhost:4200').searchParams.get('code')
     this.request.postMethod('http://localhost:3003/sign-up/githubVerigyUrl',{code}).subscribe((a)=>{
+     
       const redirectFail=()=>{
+        this.showError.displayError()
+        this.router.navigate(['/login'])
+      }
+      const successRedirect=()=>{
 
       }
-      a==='Something bad happened; please try again later.'?redirectFail:this.router.navigate(['/'])
+      a==='Something bad happened; please try again later.'?redirectFail():this.router.navigate(['/'])
     })
   }
 }
